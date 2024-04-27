be7ed7a2f4f62a0dd42015cefe8ac2f636aa3722
Remdolle <161079777+remdolle@users.noreply.github.com>
2024-04-27 17:03:18 +0200

    add prompt to ask for size grid and create a new one + randomize color for mouseover

--- app.js
+++ app.js
@@ -1,23 +1,55 @@
 let tailleGrille = 600;
-let rows = 16;
-let columns = 16;
+let rows = 10;
+let columns = rows;
+const container = document.querySelector("#container");
 
-const grille = document.querySelector("#grille");
-grille.style.width = `${tailleGrille}px`;
-grille.style.height = `${tailleGrille}px`;
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 
 
 
-function createGrid() {
+function createGrid(rows,columns) {
+    const grille = document.querySelector(".grille");
+    grille.remove();
+    const newGrid  = document.createElement("div");
+    newGrid.classList.add("grille");
+    container.appendChild(newGrid);
+
+    newGrid.style.width = `${tailleGrille}px`;
+    newGrid.style.height = `${tailleGrille}px`;
+
     for (let i=1; i<= (rows * columns); i++) {
         const cell = document.createElement("div");
-        cell.style.width = `${(tailleGrille/columns-2)}px`;
-        cell.style.height = `${(tailleGrille/rows-2)}px`;
+        cell.style.width = `${tailleGrille/columns-2}px`;
+        cell.style.height = `${tailleGrille/rows-2}px`;
         cell.classList.add("element");
-        grille.appendChild(cell);   
-    }        
-     
+        newGrid.appendChild(cell);
+        
+        cell.addEventListener("mouseover",(e)=>{
+            e.target.style.backgroundColor = getRandomColor();
+        });
+
+    }         
 }
 
-createGrid();
+
+createGrid(rows,columns);
+
+const resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click",()=>{
+    let rows = Number(prompt("enter the number of rows required : "));
+
+    while (rows > 100) {
+        rows = Number(prompt("Can't be larger than 100 ! "));
+    }
+    columns = rows;
+    createGrid(rows,columns);
+})
 
